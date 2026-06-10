1d94c3883169b59486d49ec8550077ae72f370d1
Bhargav158028 <bhargav15802808@gmail.com>
2026-06-10 20:00:00 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 #Launch an instance with 9000 and t2.medium
 cd /opt/
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
-unzip sonarqube-8.9.6.50800.zip
-yum install java-21-amazon-corretto -y
-useradd sonar
-chown sonar:sonar sonarqube-8.9.6.50800 -R
-chmod 777 sonarqube-8.9.6.50800 -R
+sudo yum install unzip -y
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
+sudo unzip sonarqube-8.9.6.50800.zip
+sudo yum install java-21-amazon-corretto -y
+sudo useradd sonar
+sudo chown -R sonar:sonar /opt/sonarqube-8.9.6.50800
+sudo chmod -R 775 /opt/sonarqube-8.9.6.50800 -R
 su - sonar
 
 #run this on server manually
